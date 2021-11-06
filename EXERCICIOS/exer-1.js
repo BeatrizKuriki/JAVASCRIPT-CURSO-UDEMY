3eac4298424e3dfca4bb77a94e73988034b36221
BeatrizKuriki <b.f.kuriki@gmail.com>
2021-11-05 22:54:45 -0300

    JS

---
+++ EXERCICIOS/exer-1.js
@@ -0,0 +1,36 @@
+// Coding Challenge
+
+//Mark and Jonh are trying to compare their BMI (Body Mass Index)
+
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / heightMark ** 2;
+let BMIJohn = massJohn / heightJohn ** 2;
+const markHigherBMI = BMIMark > BMIJohn
+
+console.log(BMIMark, BMIJohn, markHigherBMI)
+
+
+
+const firstName = "Beatriz";
+const job = "Tech Student";
+const birthYear = "1987";
+const year = 2021
+
+const Beatriz = "I'm " + firstName + ",a " + (year - birthYear) + " years old " + job + "!"
+console.log (Beatriz)
+
+//you can also write in a different way
+
+const beatrizNew = 
+//i didn't find a way to put in backticks
+
+//string with multiple lines
+
+console.log ("String with \n\
+multiple \n\
+lines");
+

fb6a6802c4565e707bdd4a3084fea293b9595688
beatriz.alencar.kuriki <b.f.kuriki@gmail.com>
2022-04-29 16:44:14 -0300

    estudos de js

--- EXERCICIOS-TAKING-DECISIONS/decisions.js
+++ EXERCICIOS-TAKING-DECISIONS/decisions.js
@@ -1,4 +1,21 @@
-let firstName = Bia;
-console.log(firstName)
+const age = 18;
 
-let
+//const isOldEnough = age>=19;
+
+if(age>=18){
+    console.log(`Beatriz can star driving license`)
+}else {
+    const yearsLeft = 18 - age;
+    console.log(`Beatriz is too young. Wait another ${yearsLeft} years.`)
+}
+
+const birthYear =2018;
+
+let century;
+if(birthYear <= 2000) {
+century = 20;
+}else{
+    century = 21;
+    
+}
+console.log(century);

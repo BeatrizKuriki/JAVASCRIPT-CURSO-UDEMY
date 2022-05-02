c56a832dd9871f6399dddbee5da45d33a1abf885
beatriz.alencar.kuriki <b.f.kuriki@gmail.com>
2022-05-02 18:34:55 -0300

    aula sobre o switch

---
+++ SWITCH-STATEMENT/switch.js
@@ -0,0 +1,43 @@
+const day  = 'tuesday';
+
+
+if(day === 'monday'){
+    console.log("Plan course structure");
+}else if (day=== 'tuesday'){
+    console.log('Prepare theory videos');
+}else if (day === 'wednesday' || day==='thursday'){
+    console.log('Write code examples');
+}else if (day === 'friday'){
+    console.log('Record Videos');
+}else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+
+}else{
+    console.log('Invalid day')
+}
+
+//switch(day){
+ //  case 'monday':
+       //console.log("Plan course structure");
+       //console.log("Go to coding meetup") ;
+      // break;
+//   case 'tuesday' :
+//       console.log('Prepare theory videos');
+//       break;
+//    case 'wednesday':
+ //   case 'thursday' :
+ //   console.log('Write code examples');
+ //   break;
+ //   case 'friday':
+ //       console.log('Record videos');
+ //   break ;  
+  //  case 'saturday':
+ //   case 'sunday':
+      //  console.log('Enjoy the weekend');  
+    //break;     
+   //default:
+     //  console.log('Not a valid day')      
+     
+//}//
+
+
